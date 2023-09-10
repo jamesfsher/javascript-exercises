dff0fe5cbeaadfc36ce8db2c3911c3c49fa365eb
James S <98997059+jamesfsher@users.noreply.github.com>
2023-09-10 10:57:58 -0500

    10 fib done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    let arr = [0, 1];
+    for (let i = 1; i < n; i++) {
+        arr.push(arr[i - 1] + arr[i]);
+    }
+    return arr[n];
 };
 
 // Do not edit below this line
